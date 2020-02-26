e68364a7f896125b9f8ca072d78c8faff6d0fb60
matcoco <b_jayson@hotmail.com>
2020-02-26 23:41:01 +0100

    crud test video done

--- back/routes/video.js
+++ back/routes/video.js
@@ -1,9 +1,131 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../config.js');
+const bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 router.get('/videos', (req, res) => {
-    res.status(200).send('toutes les videos');
+    pool.getConnection(function (err, connection){
+        connection.query(`SELECT * FROM video`, (err, results, fields) => {
+            connection.release();
+            if(err){
+                res.status(200).send(err.message);
+            }else{
+                res.status(200).send(results);
+            }
+            
+        });
+    });
+
 });
+
+router.get('/videos/:id', (req, res) => {
+    const id = req.params.id;
+    pool.getConnection(function (err, connection){
+        connection.query(`SELECT * FROM video WHERE id=?`,[id], (err, results, fields) => {
+            connection.release();
+            if(err){
+                res.status(200).send(err.message);
+            }else{
+                res.status(200).send(results);
+            }
+            
+        });
+    });
+
+});
+
+router.post('/videos', (req, res) => {
+    pool.getConnection(function (err, connection){
+
+        const formData = req.body;
+       
+        connection.query(`INSERT INTO video (url) VALUES (?)`,[formData.url], (err, results, fields) => {
+            connection.release();
+            if(err){
+                res.status(200).send(err.message);
+            }else{
+                const id = results.insertId;
+                connection.query(`SELECT url FROM video WHERE id=?`,[id], (err, results, fields) => {
+                    if(err){
+                        res.status(200).send(err.message);
+                    }else{
+                        res.json(results)
+                    }
+                });
+            }
+        });
+    });
+
+});
+
+router.put('/videos/:id', (req, res) => {
+    const id = req.params.id;
+
+    pool.getConnection(function (err, connection){
+        const formData = req.body;
+
+        connection.query(`UPDATE video SET url=? WHERE id=?`,[formData.url, id], (err, results, fields) => {
+            connection.release();
+            if(err){
+                res.status(200).send(err.message);
+            }else{
+                const id = results.insertId;
+                connection.query(`SELECT url FROM video WHERE id=?`,[id], (err, results, fields) => {
+                    if(err){
+                        res.status(200).send(err.message);
+                    }else{
+                        res.json(results)
+                    }
+                });
+            }
+        });
+    });
+});
+
+router.delete('/videos/:id', (req, res) => {
+    const id = req.params.id;
+    pool.getConnection(function (err, connection){
+        connection.query(`SELECT * FROM video WHERE id=?`,[id], (err, results, fields) => {
+            connection.release();
+            if(err){
+                res.status(200).send(err.message);
+            }else{
+                let output = results;
+                pool.getConnection(function (err, connection){
+                    connection.query(`DELETE FROM video WHERE id=?`,[id], (err, results, fields) => {
+                        if(err){
+                            res.status(200).send(err.message);
+                        }else{
+                            res.send(output);
+                        }
+                    });
+                });
+            }
+        });
+    });
+});
+
+//BBOOOOMMMM
+router.delete('/videos', (req, res) => {
+        pool.getConnection(function (err, connection){
+            connection.query('TRUNCATE TABLE video',(err, results, fields) => {
+                connection.release();
+                if(err){
+                    res.status(200).send(err.message);
+                }else{
+                    res.send("BOOOMMMMM");
+                }
+        });
+    });
+});
+
+
+
+
 
 module.exports = router;
--- back/server.js
+++ back/server.js
@@ -1,27 +1,11 @@
-const express = require('express');
-const app = express();
-const video = require('./routes/video');
-const bodyParser = require('body-parser');
+
 const connection = require('./config.js');
+const app = require('./app');
+const pool = require('./config.js');
 
-
-app.use(video);
-app.use(bodyParser);
-
-const cors = require('cors');
-
-
-const server = app.listen(5000, () => {
+app.listen(5000, () => {
     console.log(`Server is listening on 5000`);
   });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
 
-  console.log('connected as id ' + connection.threadId);
-});
 
-  module.exports = server;
